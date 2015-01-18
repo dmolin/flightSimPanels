9c43359aac8f63e8e0b3cbd20d4ee49b22a95e62
dmolin <davide.molin@gmail.com>
2015-01-18 00:28:24 +0000

    Adding comments

--- src/services/measures/speed/index.js
+++ src/services/measures/speed/index.js
@@ -1,3 +1,5 @@
 module.exports = function(config) {
+    //all the information for this instrument are contained in the x-type 3
+    //so we simply proxy and return that JSON object..
 	return config.datarefs["3"];
 };
